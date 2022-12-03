ad644481de9243a116ab387f11722a79aac9868d
Arthasee <baptiste.braun.delvoye@gmail.com>
2022-12-03 17:11:40 +0100

    modification .h et main

--- src/calculs.h
+++ src/calculs.h
@@ -20,6 +20,6 @@ double EnergiePotentielleEntreDeuxParicles(double gamma, double R, double B,
                                             double Sigma, double l, double L_c);
 double ForceBetweenTwoInteractingParticles(double gamma, double R, double B, 
                                             double Sigma, double l, double L_c);
-double ForceBord(bord_t bord, cheerio_t cheerio, double gamma, double lc, double rho_l, double rho_air, double g);
-void InitialiseBondEtSigma(cheerio_t* cheerios, int nb_cheerios, double capilary_length, double rho_liq, double rho_cheerio);
+double ForceBord(bord_t bord, cheerio_t cheerio, double surface_tension, double capilary_length);
+void InitialiseBondEtSigma(cheerio_t* cheerios, int nb_cheerios, double capilary_length, double rho_liq, double rho_cheerio, bord_t* bord);
 #endif // CALCULS_H
